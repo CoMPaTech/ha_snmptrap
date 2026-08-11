55063cb3381ada246f5795ce0ddee13ce9187cef
Tom Scholten <git@scholten.nu>
2026-08-11 08:40:41 +0000

    Ensure single listener

--- custom_components/snmptrap/config_flow.py
+++ custom_components/snmptrap/config_flow.py
@@ -12,6 +12,12 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        await self.async_set_unique_id("snmptrap_singleton")
+        self._abort_if_unique_id_configured()
+
         return self.async_create_entry(
             title="SNMP Trap Listener",
             data={},  # nothing to store
